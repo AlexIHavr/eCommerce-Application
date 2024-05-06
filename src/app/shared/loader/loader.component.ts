1fb894fa42ed4840dd91c30cd6f032301ed47f0d
alexihavr <dchavronin@bk.ru>
2024-05-06 11:29:32 +0300

    fix: remove modal component

--- src/app/shared/loader/loader.component.ts
+++ src/app/shared/loader/loader.component.ts
@@ -1,14 +1,27 @@
+import BaseComponent from 'shared/base/base.component';
 import { div } from 'shared/tags/tags.component';
-import Wrapper from 'shared/wrapper/wrapper.component';
 
 import styles from './loader.styles.module.scss';
 
-class Loader extends Wrapper {
-  constructor() {
-    super();
+class Loader extends BaseComponent {
+  private isOpened: boolean = false;
 
-    this.addClass(styles.loader);
+  constructor() {
+    super({ className: styles.loader });
+
     this.append(div({ className: styles.loaderIcon }));
+  }
+
+  public open(): void {
+    if (this.isOpened) return;
+
+    document.body.append(this.getNode());
+    this.isOpened = true;
+  }
+
+  public close(): void {
+    this.destroy();
+    this.isOpened = false;
   }
 }
 
