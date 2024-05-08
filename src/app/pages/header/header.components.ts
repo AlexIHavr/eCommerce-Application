5f2248acbe1a3b5b0ee486d663fb314f39a77c41
alexihavr <dchavronin@bk.ru>
2024-05-08 13:35:45 +0300

    feat: add localStorageService

--- src/app/pages/header/header.components.ts
+++ src/app/pages/header/header.components.ts
@@ -1,4 +1,10 @@
-import { loginNavLink, mainNavLink, signupNavLink } from 'pages/shared/navLinks/navLinks.component';
+import {
+  loginNavLink,
+  logoutNavLink,
+  mainNavLink,
+  signupNavLink,
+} from 'pages/shared/navLinks/navLinks.component';
+import { LocalStorageService } from 'services/localStorage.service';
 import { BaseComponent } from 'shared/base/base.component';
 import { div, icon } from 'shared/tags/tags.component';
 
@@ -6,10 +12,17 @@ import styles from './header.styles.module.scss';
 
 export class Header extends BaseComponent {
   constructor() {
+    const isLogined = LocalStorageService.getData('user');
+
     super(
       { tag: 'header', className: styles.header },
       div({ className: styles.logo }, icon({}, '&#128241;')),
-      div({ className: styles.navLinks }, mainNavLink, loginNavLink, signupNavLink),
+      div(
+        { className: styles.navLinks },
+        mainNavLink,
+        isLogined ? logoutNavLink : loginNavLink,
+        signupNavLink,
+      ),
       div({ className: styles.navIcons }, icon({}, '&#128187;'), icon({}, '&#128722;')),
     );
   }
--- src/app/pages/shared/navLinks/navLinks.component.ts
+++ src/app/pages/shared/navLinks/navLinks.component.ts
@@ -4,4 +4,5 @@ import { getNavigoLink } from './navLinks.helpers';
 
 export const mainNavLink = getNavigoLink('Home', PagesPaths.MAIN);
 export const loginNavLink = getNavigoLink('Login', PagesPaths.LOGIN);
+export const logoutNavLink = getNavigoLink('Logout', PagesPaths.LOGIN);
 export const signupNavLink = getNavigoLink('Signup', PagesPaths.SIGNUP);
