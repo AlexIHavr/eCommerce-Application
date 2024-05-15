fcdbe57ea24200e40dc0cf22c760e5e514190ac4
alexihavr <dchavronin@bk.ru>
2024-05-15 16:14:45 +0300

    feat: add adaptive design

--- src/app/pages/footer/footer.component.ts
+++ src/app/pages/footer/footer.component.ts
@@ -1,5 +1,5 @@
 import { BaseComponent } from 'shared/base/base.component';
-import { a, div, h2 } from 'shared/tags/tags.component';
+import { a, div, h3 } from 'shared/tags/tags.component';
 
 import styles from './footer.module.scss';
 
@@ -8,10 +8,17 @@ export class Footer extends BaseComponent {
     super(
       { tag: 'footer', className: styles.footer },
       div({ className: styles.rssLogo }),
-      a({ text: 'AlexIHavr (Team Lead)', href: 'https://github.com/AlexIHavr', target: '_blank' }),
-      a({ text: 'Koshman-Dmitri', href: 'https://github.com/Koshman-Dmitri', target: '_blank' }),
-      a({ text: 'Parxommm', href: 'https://github.com/Parxommm', target: '_blank' }),
-      h2('2024'),
+      div(
+        { className: styles.gitLinks },
+        a({
+          text: 'AlexIHavr (Team Lead)',
+          href: 'https://github.com/AlexIHavr',
+          target: '_blank',
+        }),
+        a({ text: 'Koshman-Dmitri', href: 'https://github.com/Koshman-Dmitri', target: '_blank' }),
+        a({ text: 'Parxommm', href: 'https://github.com/Parxommm', target: '_blank' }),
+      ),
+      h3('2024', styles.year),
     );
   }
 }
