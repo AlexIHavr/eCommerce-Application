f8a10197614022eccbee82c362893d8504313798
alexihavr <dchavronin@bk.ru>
2024-05-15 11:29:56 +0300

    feat: add anchor style

--- src/app/pages/footer/footer.component.ts
+++ src/app/pages/footer/footer.component.ts
@@ -1,5 +1,5 @@
 import { BaseComponent } from 'shared/base/base.component';
-import { a, div, h3 } from 'shared/tags/tags.component';
+import { a, div, h2 } from 'shared/tags/tags.component';
 
 import styles from './footer.module.scss';
 
@@ -11,7 +11,7 @@ export class Footer extends BaseComponent {
       a({ text: 'AlexIHavr (Team Lead)', href: 'https://github.com/AlexIHavr', target: '_blink' }),
       a({ text: 'Koshman-Dmitri', href: 'https://github.com/Koshman-Dmitri', target: '_blink' }),
       a({ text: 'Parxommm', href: 'https://github.com/Parxommm', target: '_blink' }),
-      h3('2024'),
+      h2('2024'),
     );
   }
 }
