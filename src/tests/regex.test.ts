93c77d76c3a763ef2014299c9c57cd6c8c606f8e
alexihavr <dchavronin@bk.ru>
2024-05-16 12:01:16 +0300

    refactor: add toBeNull

--- src/tests/regex.test.ts
+++ src/tests/regex.test.ts
@@ -6,11 +6,11 @@ describe('check login regex', () => {
     const emailPattern = LOGIN_PROPS.email.pattern ?? '';
 
     ['test@mail.ru', 'test@mail.com', 'Test_@mail.ruru'].forEach((test) => {
-      expect(test.match(emailPattern)).not.toBe(null);
+      expect(test.match(emailPattern)).not.toBeNull();
     });
 
     ['test.ru', ' test@mail.ru', 'test@mail.ru ', 'test@.ru'].forEach((test) => {
-      expect(test.match(emailPattern)).toBe(null);
+      expect(test.match(emailPattern)).toBeNull();
     });
   });
 
@@ -18,10 +18,10 @@ describe('check login regex', () => {
     const passwordPattern = LOGIN_PROPS.password.pattern ?? '';
 
     ['1234567', 'aaaaaaa1', 'AAAAAAA1', 'Aaaaaaaa', ' 12345aAa '].forEach((test) => {
-      expect(test.match(passwordPattern)).toBe(null);
+      expect(test.match(passwordPattern)).toBeNull();
     });
 
-    expect('123456Aa'.match(passwordPattern)).not.toBe(null);
+    expect('123456Aa'.match(passwordPattern)).not.toBeNull();
   });
 });
 
@@ -33,17 +33,17 @@ describe('check signup regex', () => {
     const shipCityPattern = SIGNUP_PROPS.shipCity.pattern ?? '';
 
     ['aAA1', 'a_A', '  '].forEach((test) => {
-      expect(test.match(firstNamePattern)).toBe(null);
-      expect(test.match(lastNamePattern)).toBe(null);
-      expect(test.match(bilCityPattern)).toBe(null);
-      expect(test.match(shipCityPattern)).toBe(null);
+      expect(test.match(firstNamePattern)).toBeNull();
+      expect(test.match(lastNamePattern)).toBeNull();
+      expect(test.match(bilCityPattern)).toBeNull();
+      expect(test.match(shipCityPattern)).toBeNull();
     });
 
     ['a', 'a a'].forEach((test) => {
-      expect(test.match(firstNamePattern)).not.toBe(null);
-      expect(test.match(lastNamePattern)).not.toBe(null);
-      expect(test.match(bilCityPattern)).not.toBe(null);
-      expect(test.match(shipCityPattern)).not.toBe(null);
+      expect(test.match(firstNamePattern)).not.toBeNull();
+      expect(test.match(lastNamePattern)).not.toBeNull();
+      expect(test.match(bilCityPattern)).not.toBeNull();
+      expect(test.match(shipCityPattern)).not.toBeNull();
     });
   });
 
@@ -52,13 +52,13 @@ describe('check signup regex', () => {
     const shipStreetPattern = SIGNUP_PROPS.shipStreet.pattern ?? '';
 
     ['A', '1', '_'].forEach((test) => {
-      expect(test.match(bilStreetPattern)).not.toBe(null);
-      expect(test.match(shipStreetPattern)).not.toBe(null);
+      expect(test.match(bilStreetPattern)).not.toBeNull();
+      expect(test.match(shipStreetPattern)).not.toBeNull();
     });
 
     ['  ', ''].forEach((test) => {
-      expect(test.match(bilStreetPattern)).toBe(null);
-      expect(test.match(shipStreetPattern)).toBe(null);
+      expect(test.match(bilStreetPattern)).toBeNull();
+      expect(test.match(shipStreetPattern)).toBeNull();
     });
   });
 });
