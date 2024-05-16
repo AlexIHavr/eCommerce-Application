16438c6ac89c220b3ab1a71596c884ab959009a4
Evgeny Parkhomenko <77242330+Parxommm@users.noreply.github.com>
2024-05-16 23:03:58 +0300

    Sprint 2 - Auto Login Api (#16)
    * feat: implement auto-login and logout for api
    
    * refactor: remove comments
    
    * refactor: remove unused code

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,4 @@
 import { PageWrapper } from 'pages/pageWrapper.component';
-import { apiService } from 'services/api.service';
 import { routingService } from 'services/routing.service';
 import { alertModal } from 'shared/alert/alert.component';
 import { BaseComponent } from 'shared/base/base.component';
@@ -9,10 +8,7 @@ class App {
 
   public start(): void {
     document.body.append(this.wrapper.getNode(), alertModal.getNode());
-
     routingService.updateLinks();
-
-    apiService.getProject();
   }
 }
 
