79a9a8993382934f4a09304cbc63067f5eefca99
alexihavr <dchavronin@bk.ru>
2024-05-23 08:59:30 +0300

    feat: split  components in  ProductsFilters

--- src/app/pages/catalog/components/productsFilters/productsFilters.component.ts
+++ src/app/pages/catalog/components/productsFilters/productsFilters.component.ts
@@ -1,67 +1,51 @@
+import { Div, Input } from 'globalTypes/elements';
 import { BaseComponent } from 'shared/base/base.component';
 import { button, div, form, img, input, label } from 'shared/tags/tags.component';
 
 import filterIcon from './images/filterIcon.png';
 import searchIcon from './images/searchIcon.png';
 import sortIcon from './images/sortIcon.png';
-import { PRODUCT_FILTER_PROPS } from './productsFilters.consts';
+import { PRODUCTS_FILTERS_PROPS, PRODUCTS_OPTIONS } from './productsFilters.consts';
 import { getSortField } from './productsFilters.helpers';
 import styles from './productsFilters.module.scss';
+import { OptionsType } from './productsFilters.types';
 
 export class ProductsFilters extends BaseComponent {
+  private readonly priceFromInput: Input;
+
+  private readonly priceToInput: Input;
+
+  private readonly filteredCount: Div;
+
+  private readonly inputOptions: Record<OptionsType, Input[]>;
+
   constructor() {
+    super({ className: styles.productsFilters });
+
+    this.inputOptions = { color: [], size: [] };
+
+    this.priceFromInput = input({
+      className: styles.priceInput,
+      ...PRODUCTS_FILTERS_PROPS.priceFrom,
+    });
+
+    this.priceToInput = input({
+      className: styles.priceInput,
+      ...PRODUCTS_FILTERS_PROPS.priceTo,
+    });
+
     const filterFieldForm = form(
       { className: styles.filterFieldForm },
-      div(
-        { className: styles.filterField, text: 'Price' },
-        input({ className: styles.priceInput, ...PRODUCT_FILTER_PROPS.priceFrom }),
-        input({ className: styles.priceInput, ...PRODUCT_FILTER_PROPS.priceTo }),
-      ),
+      div({ className: styles.filterField, text: 'Price' }, this.priceFromInput, this.priceToInput),
       div(
         { className: styles.filterField },
         div({ className: styles.filterFieldTitle, text: 'Size' }),
-        div(
-          { className: styles.multipleSelect },
-          label(
-            { className: styles.multipleSelectLabel, text: 'Small' },
-            input({ className: styles.selectCheckbox, value: 'S', type: 'checkbox' }),
-          ),
-          label(
-            { className: styles.multipleSelectLabel, text: 'Medium' },
-            input({ className: styles.selectCheckbox, value: 'M', type: 'checkbox' }),
-          ),
-          label(
-            { className: styles.multipleSelectLabel, text: 'Large' },
-            input({ className: styles.selectCheckbox, value: 'L', type: 'checkbox' }),
-          ),
-        ),
+        this.getMultipleSelect('size'),
       ),
       div(
         { className: styles.filterField },
         div({ className: styles.filterFieldTitle, text: 'Color' }),
-        div(
-          { className: styles.multipleSelect },
-          label(
-            { className: styles.multipleSelectLabel, text: 'Black' },
-            input({ className: styles.selectCheckbox, value: 'Black', type: 'checkbox' }),
-          ),
-          label(
-            { className: styles.multipleSelectLabel, text: 'Red' },
-            input({ className: styles.selectCheckbox, value: 'Red', type: 'checkbox' }),
-          ),
-          label(
-            { className: styles.multipleSelectLabel, text: 'Yellow' },
-            input({ className: styles.selectCheckbox, value: 'Yellow', type: 'checkbox' }),
-          ),
-          label(
-            { className: styles.multipleSelectLabel, text: 'Green' },
-            input({ className: styles.selectCheckbox, value: 'Green', type: 'checkbox' }),
-          ),
-          label(
-            { className: styles.multipleSelectLabel, text: 'White' },
-            input({ className: styles.selectCheckbox, value: 'White', type: 'checkbox' }),
-          ),
-        ),
+        this.getMultipleSelect('color'),
       ),
       button({
         className: styles.filterSubmitBtn,
@@ -71,16 +55,17 @@ export class ProductsFilters extends BaseComponent {
       }),
     );
 
-    super(
-      { className: styles.productsFilters },
-      div(
-        {
-          className: styles.filteredCount,
-          text: '5 / 20',
-          onclick: () => filterFieldForm.toggleClass(styles.show),
-        },
-        img({ className: styles.icon, src: filterIcon, alt: 'filter-icon' }),
-      ),
+    this.filteredCount = div(
+      {
+        className: styles.filteredCount,
+        text: '5 / 20',
+        onclick: () => filterFieldForm.toggleClass(styles.show),
+      },
+      img({ className: styles.icon, src: filterIcon, alt: 'filter-icon' }),
+    );
+
+    this.appendChildren([
+      this.filteredCount,
       filterFieldForm,
       div(
         { className: styles.filterField },
@@ -91,14 +76,36 @@ export class ProductsFilters extends BaseComponent {
       div(
         { className: styles.filterField },
         img({ className: styles.icon, src: searchIcon, alt: 'sort-icon' }),
-        input({ className: styles.searchInput, ...PRODUCT_FILTER_PROPS.search }),
+        input({ className: styles.searchInput, ...PRODUCTS_FILTERS_PROPS.search }),
       ),
+    ]);
+  }
+
+  private getMultipleSelect(optionsType: OptionsType): Div {
+    return div(
+      { className: styles.multipleSelect },
+      ...PRODUCTS_OPTIONS[optionsType].map((option) => {
+        const inputOption = input({
+          className: styles.selectCheckbox,
+          value: option,
+          type: 'checkbox',
+        });
+
+        this.inputOptions[optionsType].push(inputOption);
+
+        return label({ className: styles.multipleSelectLabel, text: option }, inputOption);
+      }),
     );
   }
 
   private submitFilter(event: MouseEvent): void {
     event.preventDefault();
     // TODO: SUBMIT FILTER
-    console.log(this);
+    console.log(
+      this.priceFromInput.getNode().value,
+      this.priceToInput.getNode().value,
+      this.inputOptions.size,
+      this.inputOptions.color,
+    );
   }
 }
--- src/app/pages/catalog/components/productsFilters/productsFilters.consts.ts
+++ src/app/pages/catalog/components/productsFilters/productsFilters.consts.ts
@@ -1,6 +1,6 @@
 import { FormFieldsProps } from 'pages/shared/components/formField/formField.types';
 
-export const PRODUCT_FILTER_PROPS = {
+export const PRODUCTS_FILTERS_PROPS = {
   priceFrom: {
     name: 'price-from',
     type: 'number',
@@ -8,14 +8,19 @@ export const PRODUCT_FILTER_PROPS = {
     pattern: '^[1-9][0-9]*$',
   },
   priceTo: {
-    name: 'price-from',
+    name: 'price-to',
     type: 'number',
     placeholder: 'To',
     pattern: '^[1-9][0-9]*$',
   },
   search: {
     name: 'search',
-    type: 'text',
+    type: 'search',
     placeholder: 'Search',
   },
 } as const satisfies FormFieldsProps;
+
+export const PRODUCTS_OPTIONS = {
+  size: ['S', 'M', 'L'],
+  color: ['Black', 'Red', 'Yellow', 'Green', 'White'],
+} as const satisfies Record<string, string[]>;
