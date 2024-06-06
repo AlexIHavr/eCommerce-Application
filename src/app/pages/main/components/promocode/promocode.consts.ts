d668981a0a6f326b252d39d9f655644ef78705bd
Koshman-Dmitri <123456desher@rambler.ru>
2024-06-06 16:47:51 +0300

    fix: typo

--- src/app/pages/main/components/promocode/promocode.consts.ts
+++ src/app/pages/main/components/promocode/promocode.consts.ts
@@ -2,4 +2,4 @@ export const CHAIRS_PROMOCODE = 'SUMMER';
 export const BEDS_PROMOCODE = 'COSY';
 export const CHAIRS_PROMO_TEXT = 'Get discount for all chairs in the catalog!';
 export const BEDS_PROMO_TEXT = 'Only this week discounts on beds!';
-export const PROMOCODDE_COPY_SUCCESS = 'Promo copied';
+export const PROMOCODE_COPY_SUCCESS = 'Promo copied';
