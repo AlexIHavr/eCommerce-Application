47310957865b24eff9c652a768b2d251a4cecc21
Koshman-Dmitri <123456desher@rambler.ru>
2024-06-06 15:39:31 +0300

    feat: promocode on main

--- src/app/pages/main/main.component.ts
+++ src/app/pages/main/main.component.ts
@@ -2,6 +2,8 @@ import sharedStyles from 'pages/shared/styles/common.module.scss';
 import { BaseComponent } from 'shared/base/base.component';
 import { div, h1 } from 'shared/tags/tags.component';
 
+import { CHAIRS_PROMOCODE } from './main.consts';
+import { copyPromocode } from './main.helpers';
 import styles from './main.module.scss';
 
 export class Main extends BaseComponent {
@@ -11,6 +13,20 @@ export class Main extends BaseComponent {
       div(
         { className: sharedStyles.container },
         h1('Welcome to The Furniture Store', styles.mainTitle),
+        div(
+          { className: styles.promocode },
+          div({ className: styles.promoText, text: 'Get discount for all chairs in the catalog!' }),
+          div(
+            { className: styles.promoWrapper },
+            div({ className: styles.title, text: 'Promo code' }),
+            div({
+              className: styles.code,
+              text: CHAIRS_PROMOCODE,
+              onclick: () => copyPromocode(),
+            }),
+            div({ className: styles.cover }),
+          ),
+        ),
       ),
     );
   }
---
+++ src/app/pages/main/main.consts.ts
@@ -0,0 +1,2 @@
+export const CHAIRS_PROMOCODE = 'SUMMER';
+export const PROMOCODDE_COPY_SUCCESS = 'Promo copied';
