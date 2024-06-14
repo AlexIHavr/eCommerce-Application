56a91d10aeca945a5603f25547690294e606a33f
Alex <77291278+AlexIHavr@users.noreply.github.com>
2024-06-14 16:44:51 +0300

    Sprint 4 - Testing (#36)
    * feat: add test on check clear card, add to cart and remove from cart
    
    * feat: add check on change product quantity

--- src/tests/apiSuccess.test.ts
+++ src/tests/apiSuccess.test.ts
@@ -1,4 +1,6 @@
+import { CartUpdateAction } from '@commercetools/platform-sdk';
 import { ProductsBrands, ProductsColors } from 'globalConsts/api.const';
+import { makeCartItemProps } from 'pages/cart/cart.helpers';
 import {
   getProductBrand,
   getProductColor,
@@ -162,5 +164,94 @@ describe('api success filter products', () => {
         getProductPrice(masterVariant),
       );
     });
+  });
+});
+
+describe('api success in cart', () => {
+  test('check create cart', async () => {
+    expect.assertions(0);
+
+    const { body } = await apiService.loginCustomer({
+      email: 'test@mail.ru',
+      password: 'Qwerty123',
+    });
+
+    const cartId = body.cart?.id;
+
+    if (cartId) {
+      await apiService.createAnonymousCart(crypto.randomUUID());
+    } else {
+      await apiService.createCustomerCart(body.customer.id);
+    }
+  });
+
+  test('check clear card, add to cart, change product quantity and remove from cart', async () => {
+    const { body } = await apiService.loginCustomer({
+      email: 'test@mail.ru',
+      password: 'Qwerty123',
+    });
+
+    const cartId = body.cart?.id;
+
+    if (cartId) {
+      const productsData = await apiService.getFilteredProducts({});
+      const firstProduct = productsData.body.results[0];
+      const { sku } = firstProduct.masterVariant;
+
+      if (sku) {
+        const clearActions = makeCartItemProps(body.cart).map((item) => {
+          return {
+            action: 'removeLineItem',
+            lineItemId: item.id,
+          } as CartUpdateAction;
+        });
+
+        const clearedCart = await apiService.clearCart(cartId, body.cart.version, clearActions);
+        const addProductCart = await apiService.addProductToCart(
+          cartId,
+          clearedCart.body.version,
+          sku,
+        );
+
+        const addingLineItem = addProductCart.body.lineItems.find(
+          ({ productSlug }) => productSlug?.en === firstProduct.slug.en,
+        );
+
+        expect(addingLineItem).not.toBeUndefined();
+
+        if (addingLineItem) {
+          const addingCount = 2;
+          const initPrice = addingLineItem.price.value.centAmount;
+          const updatedProductCart = await apiService.changeProductQuantity(
+            cartId,
+            addProductCart.body.version,
+            addingLineItem.id,
+            addingCount,
+          );
+
+          const updatedLineItem = updatedProductCart.body.lineItems.find(
+            ({ productSlug }) => productSlug?.en === firstProduct.slug.en,
+          );
+
+          expect(updatedLineItem).not.toBeUndefined();
+          expect(updatedLineItem?.quantity).toBe(addingCount);
+          expect(updatedLineItem?.totalPrice.centAmount).toBe(initPrice * addingCount);
+
+          if (updatedLineItem) {
+            const removedProductCart = await apiService.removeProductFromCart(
+              cartId,
+              updatedProductCart.body.version,
+              updatedLineItem.id,
+            );
+
+            const removedLineItem = removedProductCart.body.lineItems.find(
+              ({ productSlug }) => productSlug?.en === firstProduct.slug.en,
+            );
+
+            expect(removedLineItem).toBeUndefined();
+          }
+        }
+      }
+    }
   });
 });
